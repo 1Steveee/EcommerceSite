05d6af50622bf2470998fc39982739369c92d9f8
steve <truckersteve781@gmail.com>
2023-02-22 21:37:54 -0500

    started test class for adding product to cart

--- src/test/java/org/EcommerceSite/pages/RegisterPage.java
+++ src/test/java/org/EcommerceSite/pages/RegisterPage.java
@@ -1,16 +1,12 @@
 package org.EcommerceSite.pages;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.time.Duration;
-
-import static org.testng.AssertJUnit.assertEquals;
 
 public class RegisterPage {
 
